81238a2a7fc5f5f935995a4df48127e3d5e866b6
Miłosz B. <milosz.bedrylo@gmail.com>
2024-05-31 18:20:02 +0200

    combined two generators into one

--- main.go
+++ main.go
@@ -6,18 +6,19 @@ import (
 )
 
 func main() {
-	graph := myGraph.GenerateGraphIncidenceMatrix(10, 100, true)
+	graph := myGraph.GenerateRandomGraph(10, 50, false, true)
 	fmt.Println(graph.ToString())
+	fmt.Println(graph.GetEdgeCount())
 
-	test := myGraph.Dijkstra(graph, 0)
-	fmt.Println(test)
+	//test := myGraph.Dijkstra(graph, 0)
+	//fmt.Println(test)
+	//
+	//test2 := myGraph.BellmanFord(graph, 0)
+	//fmt.Println(test2)
 
-	test2 := myGraph.BellmanFord(graph, 0)
-	fmt.Println(test2)
-
-	//mst1, _ := myGraph.Kruskal(graph, true)
-	//fmt.Println(mst1.ToString())
-	////
-	//mst2, _ := myGraph.Prim(graph, 0, true)
-	//fmt.Println(mst2.ToString())
+	mst1, _ := myGraph.Kruskal(graph, true)
+	fmt.Println(mst1.ToString())
+	//
+	mst2, _ := myGraph.Prim(graph, 0, true)
+	fmt.Println(mst2.ToString())
 }
--- myGraph/generator.go
+++ myGraph/generator.go
@@ -4,9 +4,14 @@ import (
 	"math/rand"
 )
 
-// GenerateGraphIncidenceMatrix creates a random graph with the specified vertex count, edge infill percentage, and directionality using an incidence matrix representation.
-func GenerateGraphIncidenceMatrix(vertices, percentageConnected int, directed bool) *IncidenceMatrix {
-	graph := NewIncidenceMatrix()
+// GenerateRandomGraph creates a random graph with the specified vertex count, edge infill percentage, directionality, and graph type.
+func GenerateRandomGraph(vertices, percentageConnected int, directed, useIncidenceMatrix bool) Graph {
+	var graph Graph
+	if useIncidenceMatrix {
+		graph = NewIncidenceMatrix()
+	} else {
+		graph = NewPredecessorList()
+	}
 	graph.SetDirected(directed)
 
 	// Add vertices to the graph
@@ -20,101 +25,37 @@ func GenerateGraphIncidenceMatrix(vertices, percentageConnected int, directed bo
 		maxEdges /= 2
 	}
 
-	// Calculate the number of edges based on the percentage connected
+	// Calculate the desired number of edges based on the percentage connected
 	desiredEdges := maxEdges * percentageConnected / 100
+	minEdges := vertices - 1
+	if desiredEdges < minEdges {
+		desiredEdges = minEdges
+	}
 
-	// Create a fully connected graph
-	for i := 0; i < vertices; i++ {
-		for j := i + 1; j < vertices; j++ {
-			if directed {
-				graph.AddEdge(i, j, rand.Intn(100)+1)
-				graph.AddEdge(j, i, rand.Intn(100)+1)
-			} else {
-				graph.AddEdge(i, j, rand.Intn(100)+1)
-			}
+	// Ensure the graph is connected using a MST approach
+	connected := make([]bool, vertices)
+	connected[0] = true
+	edgesAdded := 0
+
+	for edgesAdded < minEdges {
+		start := rand.Intn(vertices)
+		end := rand.Intn(vertices)
+		if start != end && connected[start] && !connected[end] && !graph.IsAdjacent(start, end) {
+			graph.AddEdge(start, end, rand.Intn(100)+1)
+			connected[end] = true
+			edgesAdded++
 		}
 	}
 
-	// Randomly remove edges until the desired percentage is reached
-	for graph.GetEdgeCount() > desiredEdges {
-		edges := graph.GetAllEdges()
-		removeIndex := rand.Intn(len(edges))
-		removeEdge := edges[removeIndex]
-
-		// Check if removing this edge will disconnect the graph
-		graph.RemoveEdge(removeEdge.Start, removeEdge.End)
-		if !IsGraphConnected(graph) {
-			graph.AddEdge(removeEdge.Start, removeEdge.End, removeEdge.Weight)
+	// Add additional random edges to meet the desired edge count
+	for edgesAdded < desiredEdges {
+		start := rand.Intn(vertices)
+		end := rand.Intn(vertices)
+		if start != end && !graph.IsAdjacent(start, end) {
+			graph.AddEdge(start, end, rand.Intn(10)+1)
+			edgesAdded++
 		}
 	}
 
 	return graph
-}
-
-// GeneratePredecessorListGraph creates a random graph with the specified vertex count, edge infill percentage, and directionality using a predecessor list representation.
-func GeneratePredecessorListGraph(vertices, percentageConnected int, directed bool) *PredecessorList {
-	graph := NewPredecessorList()
-	graph.SetDirected(directed)
-
-	// Add vertices to the graph
-	for i := 0; i < vertices; i++ {
-		graph.AddVertex()
-	}
-
-	// Calculate the maximum possible edges based on the graph being directed or not
-	maxEdges := vertices * (vertices - 1)
-	if !directed {
-		maxEdges /= 2
-	}
-
-	// Calculate the number of edges based on the percentage connected
-	desiredEdges := maxEdges * percentageConnected / 100
-
-	// Create a fully connected graph
-	for i := 0; i < vertices; i++ {
-		for j := i + 1; j < vertices; j++ {
-			if directed {
-				graph.AddEdge(i, j, rand.Intn(100)+1)
-				graph.AddEdge(j, i, rand.Intn(100)+1)
-			} else {
-				graph.AddEdge(i, j, rand.Intn(100)+1)
-			}
-		}
-	}
-
-	// Randomly remove edges until the desired percentage is reached
-	for graph.GetEdgeCount() > desiredEdges {
-		edges := graph.GetAllEdges()
-		removeIndex := rand.Intn(len(edges))
-		removeEdge := edges[removeIndex]
-
-		// Check if removing this edge will disconnect the graph
-		graph.RemoveEdge(removeEdge.Start, removeEdge.End)
-		if !IsGraphConnected(graph) {
-			graph.AddEdge(removeEdge.Start, removeEdge.End, removeEdge.Weight)
-		}
-	}
-
-	return graph
-}
-
-// IsGraphConnected checks if the graph is connected
-func IsGraphConnected(graph Graph) bool {
-	visited := make(map[int]bool)
-	vertices := graph.GetVertexCount()
-
-	var dfs func(int)
-	dfs = func(v int) {
-		visited[v] = true
-		for _, neighbor := range graph.GetNeighbours(v) {
-			if !visited[neighbor] {
-				dfs(neighbor)
-			}
-		}
-	}
-
-	// Start DFS from the first vertex
-	dfs(0)
-
-	return len(visited) == vertices
 }
